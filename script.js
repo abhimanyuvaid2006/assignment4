b61ba7ba7ff047e0f2d93aa91c8f8d7b46691c04
Abhimanyu vaid <avaid2@academic.rrc.ca>
2026-06-04 16:26:58 -0500

    feat: add form validation for text and dropdown inputs

---
+++ script.js
@@ -0,0 +1,47 @@
+const form = document.getElementById("surveyForm");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const errorMessages = document.querySelectorAll(".error-message");
+    errorMessages.forEach(error => {
+        error.remove();
+    });
+
+    if (validateForm()) {
+        form.submit();
+        console.log("Validation successful");
+    } else {
+        console.log("Validation failed");
+    }
+});
+
+function validateForm() {
+    let isValid = true;
+
+    const fullNameInput = document.getElementById("fullName");
+    const fullName = escapeHTML(fullNameInput.value);
+
+    if (fullName === "") {
+        showInputError(fullNameInput, "Full name is required.");
+        isValid = false;
+    }
+
+    const frequencyInput = document.getElementById("frequency");
+    const frequency = escapeHTML(frequencyInput.value);
+
+    if (frequency === "") {
+        showInputError(frequencyInput, "Please select an option.");
+        isValid = false;
+    }
+
+    const usernameInput = document.getElementById("username");
+    const username = escapeHTML(usernameInput.value);
+
+    if (username === "") {
+        showInputError(usernameInput, "Username is required.");
+        isValid = false;
+    }
+
+    return isValid;
+}
